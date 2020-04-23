1ccc804a7d1e406d6a00df9abdfb6b0be49f2903
Naveen <Naveen.Subramaniyam@cognizant.com>
2020-04-23 11:42:35 +0530

    Router Concept

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <h1> Welcome to Vue </h1>
-    <router-view/>
-
+    <router-link to="/food">food</router-link><br>
+    <router-link to="/game">Game</router-link><br>
+    <router-link to="/sports">Sports</router-link>
+     <router-view />
   </div>
   
 </template>
 
 <script>
-
+export default {
+  name: 'app'
+}
 </script>
 
 <style>
@@ -17,7 +21,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+
   color: #2c3e50;
   margin-top: 60px;
 }
--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,40 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/home'
 import HelloWorld from '@/components/HelloWorld'
+import food from '@/components/food'
+import game from '@/components/game'
+import sports from '@/components/sports'
 
 Vue.use(Router)
 
 export default new Router({
-    route: [
+    mode: 'history',
+    routes: [
         {
             path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
-
-        },
-        {
-            path: '/home',
             name: 'Home',
             component: Home
-
+        },
+        {
+            path: '/food',
+            name: 'food',
+            component: food
+        },
+        {
+            path: '/game',
+            name: 'game',
+            component: game
+        },
+        {
+            path: '/sports',
+            name: 'sports',
+            component: sports
+        },
+        {
+            
+            path: '/home',
+            name: 'HelloWorld',
+            component: HelloWorld
         }
     ]
 })
