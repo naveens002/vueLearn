14759325c2160177c777b8fb2a68b09b9de69821
Naveen <63896835+naveens002@users.noreply.github.com>
2020-06-23 15:49:30 +0530

    Formatted document

--- src/router/store.js
+++ src/router/store.js
@@ -7,23 +7,23 @@ export const store = new Vuex.Store({
     state: {
         selectedUser: "",
         users: [
-            {name: 'Joslyn', id:'1'},
-            {name: 'Sebile', id:'2'},
-            {name: 'Remi', id:'3'},
-            {name: 'Leona', id:'4'}
+            { name: 'Joslyn', id: '1' },
+            { name: 'Sebile', id: '2' },
+            { name: 'Remi', id: '3' },
+            { name: 'Leona', id: '4' }
         ]
     },
     getters: {
         users(state) {
             return state.users;
-          },
-          selectedUser(state) {
-              return state.selectedUser;
-          }
+        },
+        selectedUser(state) {
+            return state.selectedUser;
+        }
     },
     mutations: {
-        changeUser(state,user) {
-            store.state.selectedUser = user;
+        changeUser(state, user) {
+            state.selectedUser = user;
         }
     }
 });
